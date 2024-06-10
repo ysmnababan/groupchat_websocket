e6d18f8966e837063d1ab0f78397aa2356350a24
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-06-11 05:04:46 +0700

    feat: add implementation for 2 group chat

--- main.go
+++ main.go
@@ -19,10 +19,14 @@ var upgrader = websocket.Upgrader{
 }
 
 // create maps of client, so it can handle multiple client simultaneusly
-var clients = make(map[*websocket.Conn]bool)
+// 1 clients for each group
+var clients [2]map[*websocket.Conn]bool
 
 // create channel for broadcasting message between client
-var broadcast = make(chan Message)
+// 1 channel for each group
+var broadcast [2]chan Message
+
+// = make(chan Message)
 
 type Message struct {
 	Username string `json:"username"`
@@ -33,7 +37,7 @@ type Message struct {
 // create a handler for establish a client to a websocket server
 // also responsible to broadcast message to a broadcast channel
 // this is implemented by continously reading data from client
-func handleConnections(w http.ResponseWriter, r *http.Request) {
+func handleConnections(w http.ResponseWriter, r *http.Request, channel int) {
 	//upgrade http connection to a websocket protocol
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -41,7 +45,7 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 	}
 	defer ws.Close()
 
-	clients[ws] = true
+	clients[channel][ws] = true
 	log.Println("Client: ", ws)
 
 	// read message from client and send to channel
@@ -53,33 +57,43 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Printf("error: %v", err)
 
-			//delete particular client
-			delete(clients, ws)
+			// Remove the client from the clients map if there's an error
+			delete(clients[channel], ws)
 			break
 		}
 
 		// send the message to broadcast channel
-		broadcast <- msg
+		broadcast[channel] <- msg
 	}
 }
 
 // create a handler for reading message from broadcast channel
 // after that send that message for each client to be printed on browser
-func handleMessage() {
+func handleMessage(channel int) {
 	for {
-		msg := <-broadcast
+		msg := <-broadcast[channel]
 
 		// send for each clients
-		for client := range clients {
+		for client := range clients[channel] {
 			// write message to client baack
 			err := client.WriteJSON(&msg)
 			if err != nil {
 				log.Printf("error: %v", err)
 				client.Close()
-				//delte particular client
-				delete(clients, client)
+				// Remove the client from the clients map if there's an error
+				delete(clients[channel], client)
 			}
 		}
+	}
+}
+
+// initialize for each channel
+func init() {
+	for i := range broadcast {
+		broadcast[i] = make(chan Message)
+	}
+	for i := range clients {
+		clients[i] = make(map[*websocket.Conn]bool)
 	}
 }
 
@@ -88,14 +102,21 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// add endpoint for websocket
+	// add endpoint for websocket along the way with the channel
+	// add new endpoint means creating new group chat
 	e.GET("/ws", func(c echo.Context) error {
-		handleConnections(c.Response().Writer, c.Request())
+		handleConnections(c.Response().Writer, c.Request(), 0)
+		return nil
+	})
+
+	e.GET("/ws/group2", func(c echo.Context) error {
+		handleConnections(c.Response().Writer, c.Request(), 1)
 		return nil
 	})
 
 	// run go routines for handling message
-	go handleMessage()
+	go handleMessage(0)
+	go handleMessage(1)
 
 	log.Println("http server started on :1323")
 	e.Logger.Fatal(e.Start(":1323"))
